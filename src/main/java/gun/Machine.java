9864edaec05e0096d421f25d9423b116af74de20
dmdrk1414 <seungchan141414@gmail.com>
2023-05-04 17:10:29 +0900

    :sparkles:[Feat] Machine 클래스에 Wepon 클래스 상속

--- src/main/java/gun/Machine.java
+++ src/main/java/gun/Machine.java
@@ -1,4 +1,18 @@
 package gun;
 
-public class Machine  {
+public class Machine extends Wepon {
+    public Machine() {
+        super.damege = 1;
+        super.durability = 10000;
+        super.gunSpeed = 30;
+    }
+
+    @Override
+    public String toString() {
+        return "Machine{" +
+                "damege=" + damege +
+                ", durability=" + durability +
+                ", gunSpeed=" + gunSpeed +
+                '}';
+    }
 }
