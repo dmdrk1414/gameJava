790a251267b05bcee544e061f4ea85b99e36af8b
dmdrk1414 <seungchan141414@gmail.com>
2023-05-04 17:17:54 +0900

    :sparkles:[Feat] User 클래스에 생성자 및 기본 변수 추가
    User는 총(gun)과 총관리인벤토리(gunInventory)을 이용해 총을 관리합니다.
    User는 이름과 나이를 적으십시오!

--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -1,4 +1,23 @@
 package user;
 
+import gun.Wepon;
+
+import java.util.ArrayList;
+
 public class User {
+    int age;
+    String name;
+    Wepon gun;
+    ArrayList<Wepon> gunInventory;
+
+    public User() {
+        this(0, "");
+    }
+
+    public User(int age, String name) {
+        this.age = age;
+        this.name = name;
+        this.gun = new Wepon();
+        this.gunInventory = new ArrayList<>();
+    }
 }
