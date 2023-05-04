dbac4162e4144f6e9f48c51bd43191bb0f4a57bc
dmdrk1414 <seungchan141414@gmail.com>
2023-05-04 16:16:15 +0900

    :sparkles:[Feat] Wepon 클래스 생성자 및 변수 생성

--- src/main/java/gun/Wepon.java
+++ src/main/java/gun/Wepon.java
@@ -1,4 +1,22 @@
 package gun;
 
 public class Wepon {
+    int damege;
+    int durability;
+    int gunSpeed;
+
+    public Wepon() {
+        this.damege = 1;
+        this.durability = 1000;
+        this.gunSpeed = 10;
+    }
+
+    @Override
+    public String toString() {
+        return "BasicWepon{" +
+                "damege=" + damege +
+                ", durability=" + durability +
+                ", gunSpeed=" + gunSpeed +
+                '}';
+    }
 }
