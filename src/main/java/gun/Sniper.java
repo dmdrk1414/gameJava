c79aa4bdae56f8fd9372301dd987a1e9104d3828
dmdrk1414 <seungchan141414@gmail.com>
2023-05-04 17:12:06 +0900

    :sparkles:[Feat] Sniper 클래스 Wepon 클래스 상속

--- src/main/java/gun/Sniper.java
+++ src/main/java/gun/Sniper.java
@@ -1,4 +1,18 @@
 package gun;
 
-public class Sniper {
+public class Sniper extends Wepon {
+    public Sniper() {
+        super.damege = 10;
+        super.durability = 500;
+        super.gunSpeed = 5;
+    }
+
+    @Override
+    public String toString() {
+        return "Sniper{" +
+                "damege=" + damege +
+                ", durability=" + durability +
+                ", gunSpeed=" + gunSpeed +
+                '}';
+    }
 }
